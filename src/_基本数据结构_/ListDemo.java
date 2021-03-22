0cf8bf637bfb7f0d27bd50d0e3e9a5f9fef49594
ZhangHao0810 <874993952@qq.com>
2021-03-22 11:44:22 +0800

    add new

---
+++ src/_基本数据结构_/ListDemo.java
@@ -0,0 +1,28 @@
+package _基本数据结构_;
+
+/**
+ * @author Super-Zhang
+ * @date 2021-03-22 11:21
+ *
+ *  可以精确控制元素位置的集合。 有序。
+ *
+ *  List的常⻅实现类
+ * ArrayList 是⼀个数组队列，相当于动态数组。它由数组实现，随机访问效率⾼，随机插⼊、随机删除
+ * 效率低。
+ * LinkedList 是⼀个双向链表。它也可以被当作堆栈、队列或双端队列进⾏操作。LinkedList随机访问
+ * 效率低，但随机插⼊、随机删除效率⾼。
+ * Vector 是⽮量队列，和ArrayList⼀样，它也是⼀个动态数组，由数组实现。但是ArrayList是⾮线程
+ * 安全的，⽽Vector是线程安全的。
+ * Stack 是栈，它继承于Vector。它的特性是：先进后出(FILO, First In Last Out)。
+ */
+public class ListDemo {
+//    TO DO
+}
+
+/**
+ * 栈
+ */
+class StackDemo{
+//    To Do
+
+        }
---
+++ src/_基本数据结构_/Map.java
@@ -0,0 +1,8 @@
+package _基本数据结构_;
+
+/**
+ * @author Super-Zhang
+ * @date 2021-03-22 11:33
+ */
+public class Map {
+}
