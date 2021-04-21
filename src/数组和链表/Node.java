bbf8ad84173261e60d9edd1e5d1635d6c1b91fe7
ZhangHao0810 <874993952@qq.com>
2021-04-21 21:45:25 +0800

    update Node

--- src/数组和链表/Node.java
+++ src/数组和链表/Node.java
@@ -4,14 +4,34 @@ package 数组和链表;
  * @author Super-Zhang
  * @date 2021-04-21 11:31
  */
-class Node {
+public class Node {
 
-    String vlaue;
-    Node next;
+    public String value;
+    public Node next;
 
-    Node(String vlaue, Node next) {
-        this.vlaue = vlaue;
+    public  Node(String value, Node next) {
+        this.value = value;
         this.next = next;
+    }
+
+    /**
+     * 最终返回的就是末位的node， 但是 最开始调用的的参数node是head
+     * 用的是递归思想，所以要有返回值的。
+     * @param i
+     * @param node
+     * @return
+     */
+    public static Node createNodes(Integer i, Node node) {
+        if (i <= 0) {
+//            Node last = new Node("NULL", null);
+//            node.next=last;
+//            return last;
+            return node;
+        }
+        Node next = new Node(i.toString(), null);
+        node.next = next;
+
+        return createNodes(i - 1, next);
     }
 
 }
