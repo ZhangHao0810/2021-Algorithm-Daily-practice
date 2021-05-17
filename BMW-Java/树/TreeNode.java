b3614737205a6cc99f11d7624b8354e1e5339b69
ZhangHao0810 <874993952@qq.com>
2021-05-17 14:21:30 +0800

    update TreeNode

---
+++ BMW-Java/树/TreeNode.java
@@ -0,0 +1,20 @@
+package 树;
+
+/**
+ * @author Super-Zhang
+ * @date 2021-05-17 10:33
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode leftchild;
+    TreeNode rightchild;
+    public TreeNode(int data){
+        this.val = data;
+    }
+    public TreeNode(){
+
+    }
+
+
+}
