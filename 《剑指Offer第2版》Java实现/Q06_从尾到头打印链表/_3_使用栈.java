d67588cc1d0c162fe1e5b5c3c8daa81b63713ad4
Super-Zhang <874993952@qq.com>
2023-03-27 17:03:31 +0800

    使用栈 实现翻转链表

--- 《剑指Offer第2版》Java实现/Q06_从尾到头打印链表/_3_使用栈.java
+++ 《剑指Offer第2版》Java实现/Q06_从尾到头打印链表/_3_使用栈.java
@@ -1,38 +1,44 @@
 package Q06_从尾到头打印链表;
 
-import java.util.ArrayList;
-import java.util.Stack;
+import org.junit.Test;
+
+import java.util.LinkedList;
 
 /**
  * @author ZhangHao
  * @date 2020/2/19 23:23
- * @Description：
- *  栈是后进先出,所以出栈顺序即为逆序.
- *      学会基本数据结构的使用方法.
+ * @Description： 栈是后进先出, 所以出栈顺序即为逆序.
+ * 学会基本数据结构的使用方法.
  */
 public class _3_使用栈 {
+
+
     public static void main(String[] args) {
-        LinkedList<Integer> list=new LinkedList<>();
-        for (int i=0;i<10;i++){
-            list.add(i);
+
+        LinkedList<Integer> linkedList = new LinkedList();
+        for (int i = 0; i < 10; i++) {
+            linkedList.add(i);
         }
 
-        ArrayList<Integer> out = printListFromTailToHead(list.getHead());
-        System.out.println(out);
+        System.out.println("链表值：" + linkedList);
+        printListFromTailToHead(linkedList);
 
     }
 
-     private static ArrayList<Integer> printListFromTailToHead(Node listNode) {
-        Stack<Integer> stack = new Stack<>();
-         while (listNode != null) {
-             stack.add((Integer) listNode.data);
-             listNode=listNode.next;
-         }
-         ArrayList<Integer> ret = new ArrayList<>();
-         while (!stack.isEmpty()) {
-             ret.add(stack.pop());
-         }
-         return ret;
+    /**
+     * @description 入栈，再出栈，即可翻转链表。
+     * @author zh
+     * @data 2023/3/27
+     */
+    private static void printListFromTailToHead(LinkedList<Integer> linkedList) {
+        LinkedList<Integer> stack = new LinkedList<>();
+        for (int i = 0; linkedList != null && i < linkedList.size(); i++) {
+            stack.push(linkedList.get(i));
+        }
+
+        while (stack.peek() != null) {
+            System.out.print(stack.pop() + ", ");
+        }
     }
 
 }
